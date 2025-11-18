2dcfc55b7ffebbb7d8fdc79f6018c3c62ad64e26
TiffanyYongNgikChee <G00425067@atu.ie>
2025-11-18 11:11:30 +0000

    Add build files and installation script for helib_wrapper

---
+++ helib_wrapper/build/CMakeFiles/helib_wrapper.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helib_wrapper.dir/src/helib_wrapper.cpp.o"
+  "CMakeFiles/helib_wrapper.dir/src/helib_wrapper.cpp.o.d"
+  "libhelib_wrapper.pdb"
+  "libhelib_wrapper.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helib_wrapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
